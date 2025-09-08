99bb1d905a6d3f8de82c560b0168fb33e7cb367c
CredenceApp <withcredence@gmail.com>
2025-09-08 12:32:49 +0100

    changes made to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
     <div>
           <div style={divStyle}>
         <div className="flex flex-col m-2 md:text-5xl text-left items-start justify-center gap-4 font-bold font-sans w-full md:w-1/2">
-          <p className='text-white text-left text-4xl font-bold'>Welcome To Dubai Royal Council <span className='text-green-600 text-center'>We Appreciate you here</span></p>
+          <p className='text-white text-left text-4xl font-bold'>Welcome To Dubai Royal Council Cabinet <span className='text-green-600 text-center'>We Appreciate you here</span></p>
           <span className="text-slate-200 text-left text-2xl font-bold">
 نحن نحب دبي
           </span>
